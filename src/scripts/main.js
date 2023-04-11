45c2192e375601b23ebd23018ce15f767997c2a0
Mark Lawson <mark@walkonmars.co.uk>
2023-04-11 14:05:53 +0100

    navbar responsive at 768px

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
-const navList = document.querySelector(".nav__list");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("nav__open");
+  if (nav.classList.contains("nav__open")) {
+    hamburger.innerHTML = "&times;";
+  } else {
+    hamburger.innerHTML = "&#9776;";
+  }
   document.body.classList.toggle("no-scroll");
 });
